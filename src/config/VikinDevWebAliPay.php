937781771010616d695e3b87a03e780e44065a2d
vikin <986465329@qq.com>
2016-10-22 17:36:50 +0800

    new alipay project

--- src/config/VikinDevWebAliPay.php
+++ src/config/VikinDevWebAliPay.php
@@ -2,19 +2,16 @@
 return [
 
     //服务器异步通知页面路径  需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    'notify_url'        => "http://www.kmsc.cc/pay/success",
+    'notify_url'        => '',
 
     //页面跳转同步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
-    'return_url'        => "http://www.kmsc.cc/pay/success",
+    'return_url'        => '',
 
     //签名方式
     'sign_type'         => strtoupper('RSA'),
 
     //字符编码格式 目前支持 gbk 或 utf-8
     'input_charset'     => strtolower('utf-8'),
-
-    //ca证书路径地址，用于curl中ssl校验
-    'cacert'            => getcwd() . '/../app/Extend/Alipay/cacert.pem',
 
     //访问模式,根据自己的服务器是否支持ssl访问，若支持请选择https；若不支持请选择http
     'transport'         => 'http',
